37be53db2a628295dec258708e494cd8cf245fe3
krushndayshmookh <krushndayshmookh@gmail.com>
2019-09-20 10:38:16 +0530

    update model

--- models/record.js
+++ models/record.js
@@ -3,35 +3,45 @@ var moment = require('moment')
 
 var Schema = mongoose.Schema
 
-var RecordSchema = new Schema({
-	date: {
-		type: Date,
-		//required: true,
-		default: ''
+var RecordSchema = new Schema(
+	{
+		date: {
+			type: Date,
+			//required: true,
+			default: ''
+		},
+		fuel: {
+			type: Number,
+			min: 0,
+			max: 100,
+			default: 0
+		},
+		vehicle: {
+			type: String,
+			default: ''
+		},
+		lat: {
+			type: Number,
+			default: 0
+		},
+		lng: {
+			type: Number,
+			default: 0
+		},
+		speed: {
+			type: Number,
+			default: 0
+		}
 	},
-	fuel: {
-		type: Number,
-		min: 0,
-		max: 100,
-		default: 0
-	},
-	vehicle: {
-		type: String,
-		default: ''
-	},
-	lat: {
-		type: Number,
-		default: 0
-	},
-	lng: {
-		type: Number,
-		default: 0
-	},
-	speed: {
-		type: Number,
-		default: 0
+	{
+		toObject: {
+			virtuals: true
+		},
+		toJSON: {
+			virtuals: true
+		}
 	}
-})
+)
 
 //Export model
 module.exports = mongoose.model('Record', RecordSchema)
