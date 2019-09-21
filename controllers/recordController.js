61509537dac87eb2c1715ade98969767630a259b
krushndayshmookh <krushndayshmookh@gmail.com>
2019-09-21 16:17:55 +0530

    change filter from registration->id

--- controllers/recordController.js
+++ controllers/recordController.js
@@ -3,15 +3,15 @@ const Vehicle = require('../models/vehicle')
 const moment = require('moment')
 
 exports.record_create_get = async (req, res) => {
-	let data = JSON.parse(
-		req.query.data
-			.split("'")
-			.join('')
-			.split(':,')
-			.join(':0,')
-	)
+  let data = JSON.parse(
+    req.query.data
+      .split("'")
+      .join('')
+      .split(':,')
+      .join(':0,')
+  )
 
-	/*
+  /*
         let data = [
             {
                 lat: 0,
@@ -26,47 +26,53 @@ exports.record_create_get = async (req, res) => {
         ]
     */
 
-	let vehicle = await Vehicle.findOne({ registration: data[0].vehicle.toUpperCase() })
+  let vehicle = await Vehicle.findOne({
+    registration: data[0].vehicle.toUpperCase()
+  })
 
-	let records = []
+  let records = []
 
-	data.forEach(state => {
-		let record = new Record({
-			date: state.date == 0 ? moment() : moment(state.date + ' ' + state.time + state.timeflg, 'DD-MM-YYYY hh:mm:ssA'),
-			fuel: state.fuel,
-			vehicle: vehicle._id,
-			lat: state.lat,
-			lng: state.lng,
-			speed: state.speed
-		})
+  data.forEach(state => {
+    let record = new Record({
+      date:
+        state.date == 0
+          ? moment()
+          : moment(
+              state.date + ' ' + state.time + state.timeflg,
+              'DD-MM-YYYY hh:mm:ssA'
+            ),
+      fuel: state.fuel,
+      vehicle: vehicle._id,
+      lat: state.lat,
+      lng: state.lng,
+      speed: state.speed
+    })
 
-		records.push(record)
+    records.push(record)
 
-		console.log(record)
-	})
+    console.log(record)
+  })
 
-	Record.insertMany(records)
-		.then(inserted => {
-			return res.send({ insertedCount: inserted.length })
-		})
-		.catch(err => {
-			console.error(err)
-			return res.status(500).send(err)
-		})
+  Record.insertMany(records)
+    .then(inserted => {
+      return res.send({ insertedCount: inserted.length })
+    })
+    .catch(err => {
+      console.error(err)
+      return res.status(500).send(err)
+    })
 }
 
 exports.record_latest_get = async (req, res) => {
-	let vehicle = await Vehicle.findOne({ registration: req.params.vehicle })
-
-	Record.find({ vehicle: vehicle._id })
-		.sort({ date: -1 })
-		.limit(1)
-		.exec((err, record) => {
-			if (err) {
-				console.error(err)
-				return res.status(500).send(err)
-			}
-			if (!record.length) return res.send({})
-			return res.send(record[0])
-		})
+  Record.find({ vehicle: req.params.vehicle })
+    .sort({ date: -1 })
+    .limit(1)
+    .exec((err, record) => {
+      if (err) {
+        console.error(err)
+        return res.status(500).send(err)
+      }
+      if (!record.length) return res.send({})
+      return res.send(record[0])
+    })
 }
